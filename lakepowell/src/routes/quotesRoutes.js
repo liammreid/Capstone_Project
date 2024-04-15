34188e351e42d38dee97601984da9e2e08dd5a75
Liam Reid <94395262+liammreid@users.noreply.github.com>
2024-04-15 14:13:07 -0600

    Delete lakepowell directory

--- lakepowell/src/routes/quotesRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const quotesController = require("../controllers/quotesController");
-
-router.get('/random-quote', quotesController.getRandomQuote);
-
-module.exports = router;
